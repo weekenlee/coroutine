3f4649bcd775ba0b9cceaf15320036d19cca0e0e
root <root@localhost.localdomain>
2018-02-09 15:40:24 +0800

    create

---
+++ coroutine.h
@@ -0,0 +1,16 @@
+#ifndef C_COROUTINE_H
+#define C_COROUTINE_H
+
+#define COROUTINE_DEAD 0
+#define COROUTINE_READY 1
+#define COROUTINE_RUNNING 2
+#define COROUTINE_SUSPEND 3
+
+struct schedule;
+
+typedef void (*coroutine_func)(struct schedule* , void *ud);
+
+struct schedule *coroutine_open(void);
+void coroutine_close(struct schedule *);
+
+#endif
